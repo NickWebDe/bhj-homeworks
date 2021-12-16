f55faf524a6bcedca232af90c21c0636a75321a2
Nikita Lashik <lnv.mfg@gmail.com>
2021-12-16 15:28:37 +0300

    Домашнее задание Fix

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,19 +1,42 @@
 let element;
-const sizeHole = 9;
+let arrHole = [];
+const sizeHole = 8;
+let killCount = document.getElementById('dead');
+let lossCounter = document.getElementById('lost')
+
+
 
 function getHole(index) {
 	element = document.getElementById(`hole${index}`);
 	return element;
 };
 
-for(let i = 1; i < sizeHole; i++) {
-	element = getHole(i);
-	console.log(element);
-	element.onclick = function() {
-		console.log(`клик ${element}`) //Здесь у меня выводится элемент с классом holle8 хотя по моей задумке должен выводится элемент на который кликаю
-    if(element.classList.contains('hole_has-mole')) {
-    	console.log('Попадание');
-    }
-  }
+
+function checkWin () {
+	if (killCount.textContent === '10') {
+		alert('Ура Победа!');
+		killCount.textContent = '0';
+	} else if (lossCounter.textContent === '5') {
+		alert('Поражение, попробуйте снова! ;)')
+		lossCounter.textContent = '0';
+	}
 }
+
+for(let i = 0; i < sizeHole; i++) {
+	element = getHole((i + 1));
+    arrHole[i] = element;
+    arrHole[i].onclick = function() {
+    	if(arrHole[i].classList.contains('hole_has-mole')) {
+    	killCount.textContent ++; /* Здесь скорее всего я сделал костыль ввиду того, что происходит неявное преобразование
+    	получается JS сам преобразует строку в число для сложения. По правильному здесь нужно было бы превести строку через
+    	метод .Number() но в таком случае пришлось бы делать еще одну лишню перменную, как лучше быть в таких ситуациях? */
+    	checkWin();
+    	return;
+    }
+    
+    lossCounter.textContent ++;
+    checkWin()
+  }
+  
+ }
 
