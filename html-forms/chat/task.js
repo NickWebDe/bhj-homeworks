19897c8392f9d6ebd64603c45d529d883006f24a
Nikita Lashik <lnv.mfg@gmail.com>
2022-01-17 00:33:49 +0300

    Homework html.form chat

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,20 +1,38 @@
-const chat = document.querySelector('.chat-widget__area');
+const botMsgArr = ['Мы не работаем!', 'Мы заняты, давайте в другой раз!', 'Напишите позже!', 'Досвидание!', 'Аривидерчи!', 'Не пишите нам больше!'];
+const input = document.getElementById('chat-widget__input');
+let clientMessageArr = document.getElementsByClassName("message message_client");
+document.querySelector('.chat-widget').addEventListener('click', (e) => e.currentTarget.className = 'chat-widget chat-widget_active');
 
-document.querySelector('.chat-widget').onclick = function (e) {
-	e.currentTarget.className = 'chat-widget chat-widget_active';
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
 }
 
-const input = document.getElementById('chat-widget__input');
 
-input.onchange = function (e) {
-  document.querySelector('.chat-widget__messages').innerHTML += 
-	`<div class="message message_client">
-    <div class="message__time">09:21</div>
-    <div class="message__text"></div>
-  </div>
-`;
-document.querySelector('.message__text').textContent = input.value;
-return false;
-};
+input.addEventListener('change', () => {
+  document.getElementById('chat-widget__messages').innerHTML += 
+  `<div class="message message_client">
+     <div class="message__time"></div>
+     <div class="message__text"></div>
+   </div>
+ `;
+
+ let clMsgLst = document.querySelectorAll('.message.message_client');
+ clMsgLst[clMsgLst.length - 1].lastElementChild.textContent = input.value;
+
+ input.value = '';
 
 
+ setTimeout(() => { 
+  document.getElementById('chat-widget__messages').innerHTML += 
+  `<div class="message">
+     <div class="message__time"></div>
+     <div class="message__text"></div>
+   </div>
+ `;
+ let botMsgLst = document.querySelectorAll('.message');
+ botMsgLst[botMsgLst.length - 1].lastElementChild.textContent = botMsgArr[randomInteger(0, botMsgArr.length)] 
+ },
+   1000)
+});
