b445f2a86c8ca0827dbe37a4dc5fef9be40b0e6f
Nikita Lashik <lnv.mfg@gmail.com>
2022-02-01 00:06:46 +0300

    Homework_Document_Structure_Fix

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,19 +1,13 @@
-
 const productControlInc = Array.from(document.getElementsByClassName('product__quantity-control product__quantity-control_inc'));
 const productControlDec = Array.from(document.getElementsByClassName('product__quantity-control product__quantity-control_dec'));
 const productAdd = Array.from(document.getElementsByClassName('product__add'));
 const cartProducts = document.querySelector('.cart__products');
-const productArr = document.getElementsByClassName('.cart__products');
-
-let htmlBasket = `<div class="cart__product" data-id="1">
-    <img class="cart__product-image" src="image.png">
-    <div class="cart__product-count">20</div>
-</div>`;
-
+const cartProduct = document.getElementsByClassName('cart__product');
+const minCount = 1;
 
 productControlDec.forEach( (e) => {
 	e.addEventListener('click', (event) => {
-		event.currentTarget.nextElementSibling.innerText <= 0 ? alert('Выберите колличетсво товара') 
+		event.currentTarget.nextElementSibling.innerText <= minCount ? alert('Выберите колличетсво товара не меньше одного')
 		: event.currentTarget.nextElementSibling.innerText --;
 	})
 })
@@ -26,11 +20,26 @@ productControlInc.forEach( (e) => {
 	})
 })
 
-productAdd.forEach( (e) => {
-	e.addEventListener('click', (event) => {
+
+
+productAdd.forEach( (element) => {
+	element.addEventListener('click', (event) => {
+		let dataId = Number(event.currentTarget.closest('.product').getAttribute('data-id'));
+		let img = event.currentTarget.closest('.product').querySelector('img').getAttribute('src');
+		let count = Number(event.currentTarget.previousElementSibling.querySelector('.product__quantity-value').innerText);
+		let htmlBasket = `<div class="cart__product" data-id=" ` + dataId +` ">
+            <img class="cart__product-image" src="` + img +`">
+            <div class="cart__product-count">` + count + `</div>
+            </div>`;
+
+		for(let i= 0; i <= cartProduct.length -1; i++) {
+			if(Number(cartProduct[i].getAttribute('data-id')) === dataId) {
+				cartProduct[i].querySelector('.cart__product-count').innerText =
+					Number(cartProduct[i].querySelector('.cart__product-count').innerText) + count;
+				return
+			}
+		}
 		cartProducts.insertAdjacentHTML('beforeEnd', htmlBasket);
-		console.log(productArr);
-		// cartProducts.querySelector('.cart__product-count').innerText = event.currentTarget.previousElementSibling.querySelector('.product__quantity-value').innerText;
-		// // // console.log(document.querySelector('.cart__product'))
+
 	})
 })
