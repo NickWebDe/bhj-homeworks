19896da2639ed0409a7dd160353ccb9e565cf967
Nikita Lashik <lnv.mfg@gmail.com>
2022-01-31 16:42:18 +0300

    'Homework_fix_ToDo'

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,26 +1,20 @@
 const taskInput = document.getElementById('task__input');
-const taskList = document.getElementById('tasks__list');
-const taskTitleArr = document.getElementsByClassName('task__title');
 const removeTask = document.getElementsByClassName('task__remove');
-const htmlTask = `<div class="task">
-  <div class="task__title">
-  </div>
-  <a href="#" class="task__remove">&times;</a>
-</div>`
 
 taskInput.addEventListener('change', (e) => {
-	e.preventDefault();
-	taskList.insertAdjacentHTML('beforeEnd', htmlTask);
-	taskTitleArr[taskTitleArr.length -1].append(taskInput.value);
+	document.getElementById('tasks__form').addEventListener('submit', (e) => e.preventDefault() );
+	let htmlElement = document.createElement('div');
+	htmlElement.innerHTML = `<div class="task">
+     <div class="task__title"> `+ taskInput.value + `
+     </div>
+     <a href="#" class="task__remove">&times;</a>
+    </div>`
+	document.getElementById('tasks__list').insertAdjacentElement("beforeend",htmlElement );
+	document.getElementById('tasks__form').reset();
 	removeTask[removeTask.length -1].addEventListener('click', (e) => {
-	e.currentTarget.closest('div').remove()
-   })
-
-	// taskInput.reset() - почему то такой подход не работает для очистки поля ввода :(
-
-	taskInput.value = '';
+ 	e.currentTarget.closest('div').remove()
+    })
 })
-
 
 
 
