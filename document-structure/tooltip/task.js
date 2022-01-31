270e083e0a103f166258c04ac3fb33d4f5c674a3
Nikita Lashik <lnv.mfg@gmail.com>
2022-02-01 01:30:37 +0300

    Homework fix

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,9 +1,22 @@
-const help = document.querySelectorAll('.has-tooltip')
-const html = '<div class="tooltip">Текст подсказки</div>'; 
-help.forEach((e) => {
-	e.addEventListener('click', (e) => {
-		e.preventDefault()
-		e.currentTarget.insertAdjacentHTML('afterEnd', html);
+const tooltipElements = Array.from(document.getElementsByClassName('has-tooltip'));
+const htmlSample = `<div class="tooltip" style="left: 0; top: 0">Текст подсказки</div>`;
+document.querySelector('.has-tooltip').insertAdjacentHTML("beforeend", htmlSample);
+const toolTip = document.querySelector('.tooltip')
+const fixPositionPx = 25;
+
+
+	tooltipElements.forEach((element) => {
+		element.addEventListener('click', (event) => {
+			event.preventDefault();
+			if(toolTip.className === 'tooltip') {
+				toolTip.className = 'tooltip tooltip_active';
+				toolTip.innerText = event.currentTarget.title;
+				toolTip.style.top = `${event.currentTarget.getBoundingClientRect().top + fixPositionPx}px`;
+				toolTip.style.left = `${event.currentTarget.getBoundingClientRect().left}px`;
+				console.log(toolTip.style.top)
+				return;
+			} toolTip.className = 'tooltip';
+			console.log(event.currentTarget.getBoundingClientRect().top)
+		})
 	})
 
-});
