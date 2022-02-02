e007b9f1d3db8a7d2bc05705fbc7c1c7485bc0c2
Nikita Lashik <lnv.mfg@gmail.com>
2022-02-02 22:40:41 +0300

    Homework_clientState

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
         if (xhr.readyState === xhr.DONE) {
             let element = JSON.parse(xhr.response).data;
             poolTitle.innerText = element.title;
-            poolAnswer.innerHTML = `<button class="poll__answer">d</button>
-                  <button class="poll__answer">a</button>
-                  <button class="poll__answer">?</button>`;
+            poolAnswer.innerHTML = `<button class="poll__answer"></button>
+                  <button class="poll__answer"></button>
+                  <button class="poll__answer"></button>`;
             const poolElements = Array.from(document.getElementsByClassName('poll__answer'));
             poolElements.forEach((e, idx) => {
                 e.innerText = element.answers[idx];
--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,18 +1,53 @@
-const btn = document.querySelector('.signin_active');
-let login = document.getElementsByName('login');
+const btnLogin = document.querySelector('.btn');
+const btnLogout = document.getElementById('logout_btn');
+const formInput = Array.from(document.querySelectorAll('input'));
 
-btn.addEventListener('submit', (e) => {
-    // let xhr = new XMLHttpRequest();
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    e.preventDefault();
+    if(localStorage.getItem('id')){
+        console.log(localStorage.getItem('id'))
+        document.getElementById('welcome').className = 'welcome welcome_active';
+        document.getElementById('user_id').innerText = localStorage.getItem('id');
+        document.getElementById('signin').className = 'signin';
+        btnLogout.className = 'btn_logout_active';
+
+    }
+})
+
+btnLogout.addEventListener('click', (e) => {
+    document.getElementById('signin').className = 'signin signin_active';
+    document.getElementById('welcome').className = 'welcome';
+    btnLogout.className = 'btn_logout';
+    localStorage.clear();
+})
+
+btnLogin.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log(login[0].value)
-    // xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php',false );
-    let dataUser = new FormData();
-    dataUser.append("1", "22")
-    // xhr.send()
-    // elem.forEach((e, idx) => {
-    //     login.append(e,e)
-        console.log(dataUser)
+    let userData = new FormData();
+    formInput.forEach((element) => {
+        userData.append(element.name, element.value);
+    })
 
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+    xhr.send(userData);
 
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === xhr.DONE) {
+            let xhrResp = JSON.parse(xhr.response);
+            if(xhrResp.success) {
+                localStorage.setItem('id', xhrResp.user_id)
+                document.getElementById('welcome').className = 'welcome welcome_active';
+                document.getElementById('user_id').innerText = xhrResp.user_id;
+                document.getElementById('signin').className = 'signin';
+                btnLogout.className = 'btn_logout_active';
+                console.log(localStorage.getItem('id'))
+                return;
+            }
+            alert('Не верные логин или пароль');
+            document.getElementById('signin__form').reset();
+        }
+    }
 })
 
