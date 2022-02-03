f22f10f24d93e4cf1479a276aa1e46e29ac4ebc2
NickWebDe <84226618+NickWebDe@users.noreply.github.com>
2022-02-03 17:35:09 +0300

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,13 +1,13 @@
 const taskInput = document.getElementById('task__input');
 const removeTask = document.getElementsByClassName('task__remove');
 const tasksForm = document.getElementById('tasks__form');
+const titleElements = document.getElementsByClassName('task__title');
 
 tasksForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	let htmlElement = document.createElement('div');
 	htmlElement.innerHTML = `<div class="task">
-     <div class="task__title"> `+ taskInput.value + `
-     </div>
+     <div class="task__title"></div>
      <a href="#" class="task__remove">&times;</a>
     </div>`
 
@@ -16,6 +16,7 @@ tasksForm.addEventListener('submit', (e) => {
 	}
 
 	document.getElementById('tasks__list').insertAdjacentElement("beforeend",htmlElement );
+	titleElements[titleElements.length - 1].textContent = taskInput.value;
 	tasksForm.reset();
 
 	removeTask[removeTask.length -1].addEventListener('click', (e) => {
