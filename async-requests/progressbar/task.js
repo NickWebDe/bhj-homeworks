4c1a766c9cf9be118aa077af8435683added4847
Nikita Lashik <lnv.mfg@gmail.com>
2022-02-06 14:44:04 +0300

    Homework_Async

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,14 +1,16 @@
-const btnSend = document.getElementById('send');
 const form = document.getElementById('form');
+const progressBar = document.getElementById('progress');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const dataFile = new FormData();
-    dataFile.append('file',document.getElementById('file').files)
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
     xhr.upload.onprogress = (e) => {
-        console.log(e.loaded)
+        progressBar.max = e.total;
+        progressBar.value = e.loaded;
     };
-    xhr.send(dataFile);
+    xhr.upload.onload = () => {
+        form.querySelector('.input__wrapper.input__wrapper-desc').textContent = 'Файл успешно загружен..';
+    }
+    xhr.send(new FormData(form));
 })
